e30822d2f4d72dc1ce951f8e9ef5ec33d823be5b
Renuka Dhane <renuka.dhane@gmail.com>
2022-09-23 00:22:28 +0530

    add tests

--- template.py
+++ template.py
@@ -22,6 +22,9 @@ list_of_files=[
     f"src/{package_name}/pipeline/__init__.py",
     f"src/{package_name}/entity/__init__.py",
     f"src/{package_name}/constants/__init__.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
     "configs/config.yaml",
     #DVC introduces a mechanism to capture data pipelines — series of data processes that produce a final result.
     "dvc.yaml",
